96af1cf52aada20a088454d66da5ded8c94923dd
ZCH <chzhch2023@163.com>
2024-05-10 16:01:08 +0800

    Site updated: 2024-05-10 16:01:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/10/hello-world/"];function toRandomPost(){
+var posts=["2024/05/10/Virtualization/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
